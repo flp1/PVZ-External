3699d25994de02ce975d615abf50db5c3869bb8f
flp1 <felipe3f@hotmail.com>
2021-03-18 17:56:42 -0300

    Add project files.

---
+++ HackAnyGame2/proc.h
@@ -0,0 +1,10 @@
+#include <vector>
+#include <Windows.h>
+#include <Tlhelp32.h>
+
+DWORD GetProcId(const wchar_t* procName);
+
+uintptr_t GetModuleBaseAddress(DWORD procId, const wchar_t* modName);
+
+uintptr_t FindDMAAddy(HANDLE hProc, uintptr_t ptr, std::vector<unsigned int> offsets);
+
